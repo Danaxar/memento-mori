376b498de5dd34ce5d72428237d097bd47f42ae1
Daniel <daniel.catalan@usach.cl>
2023-07-30 23:42:46 -0400

    feat: input fecha de nacimiento usuario

--- main.js
+++ main.js
@@ -11,45 +11,55 @@ for(let i = 0; i < filas; i++){
 }
 
 // Obtener la fecha de nacimiento
-const fechaNacimiento = localStorage.getItem("nac");
+const inputDate = document.querySelector("input[type='date']")
+var fechaNacimiento;
+inputDate.addEventListener("change", () => {
+  fechaNacimiento = new Date(inputDate.value)
+})
 if(fechaNacimiento == null){
   alert('Ingresa tu fecha de nacimiento')
 }
 
-// Obtener la fecha actual
-const fecha = new Date();
+const verButton = document.querySelector("button")
+verButton.addEventListener("click", () => {
+  // Obtener la fecha actual
+  const fecha = new Date();
 
-// Calcular las semanas vividas
-const diasVividos = parseInt((fecha - fechaNacimiento) / (1000 * 60 * 60 * 24));
-var semanasVividas = parseInt(diasVividos / 7)
-console.log("🚀 ~ file: main.js:15 ~ semanasVividas:", semanasVividas)
-// Insertar en la matriz
-for(let i = 0; i < filas && semanasVividas > 0; i++){
-  for(let j = 0; j < columnas && semanasVividas > 0; j++){
-    if(semanasVividas > 0){
-      matrix[i][j] = 1;
-      semanasVividas--;
+  // Calcular las semanas vividas
+  const diasVividos = parseInt((fecha - fechaNacimiento) / (1000 * 60 * 60 * 24));
+  var semanasVividas = parseInt(diasVividos / 7)
+  console.log("🚀 ~ file: main.js:15 ~ semanasVividas:", semanasVividas)
+  // Insertar en la matriz
+  for(let i = 0; i < filas && semanasVividas > 0; i++){
+    for(let j = 0; j < columnas && semanasVividas > 0; j++){
+      if(semanasVividas > 0){
+        matrix[i][j] = 1;
+        semanasVividas--;
+      }
     }
   }
-}
 
-// Modificar la tabla en el dom
-const table = document.getElementById("table")
-const tbody = document.createElement("tbody")
-table.appendChild(tbody)
-for(let i = 0; i < filas; i++){
-  const tr = document.createElement("tr")
-  for(let j = 0; j < columnas; j++){
-    const td = document.createElement("td")
-    td.textContent = '0'
-    console.log(td)
-    if(matrix[i][j] == 1){
-      td.classList.add("alive")
-    }else{
-      td.classList.add("dead")
-    }
-    tr.appendChild(td)
+  // Modificar la tabla en el dom
+  const table = document.getElementById("table")
+  // Quitar tbody si existe
+  if(table.children.length > 0){
+    table.removeChild(table.children[0])
   }
-  tbody.appendChild(tr)
-}
-console.log(matrix)
+  const tbody = document.createElement("tbody")
+  table.appendChild(tbody)
+  for(let i = 0; i < filas; i++){
+    const tr = document.createElement("tr")
+    for(let j = 0; j < columnas; j++){
+      const td = document.createElement("td")
+      td.textContent = '0'
+      if(matrix[i][j] == 1){
+        td.classList.add("alive")
+      }else{
+        td.classList.add("dead")
+      }
+      tr.appendChild(td)
+    }
+    tbody.appendChild(tr)
+  }
+  console.log(matrix)
+})
